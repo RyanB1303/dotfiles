9361c16d7e4a4c871df05b1313b5cee4adf8d8f2
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-02-21 22:56:08 +0700

    doom with kawai splash

--- doom/config.el
+++ doom/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Ryan B"
+(setq user-full-name "RyanB1303"
       user-mail-address "ryan.brilliant.nirwana@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
@@ -21,9 +21,9 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-                                        ; (setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'semi-light)
-                                        ;       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-(setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'semi-light))
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -32,19 +32,47 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-palenight)
+;; fav theme: doom-moonlight leuven
+(setq doom-theme 'leuven
+      doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 15)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 16)
+      nerd-icons-font-names '("SymbolsNerdFontMono-Regular.ttf"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
+
+;; Prevents some cases of Emacs flickering.
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
+;; fullscreen at start - repot
+;; (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;;; :ui doom-dashboard
+(setq fancy-splash-image (file-name-concat doom-user-dir "splash.png"))
+
+;; projectile dir
+(setq projectile-project-search-path '(("~/Projects/" . 3) ("~/Exercism/" . 3)))
+
+;;; :ui modeline
+;; An evil mode indicator is redundant with cursor shape
+(setq doom-modeline-modal nil)
+
+;;; :editor evil
+;; Focus new window after splitting
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Projects/org/"
+      deft-directory "~/Projects/org/")
 
-;; Projectile search
-(setq projectile-project-search-path '(("~/Projects/" . 3)))
+;; org todo set done time
+(after! org
+  (setq org-log-done 'time))
 
+;; which-key scroll
+(setq which-key-use-C-h-commands 't)
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
@@ -76,14 +104,16 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-;; (add-to-list lsp-language-id-configuration
-                                        ;   '((".*\\.html\\.erb$" . "html")))
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
-;; autoformat
-(add-hook 'before-save-hook #'+format/buffer nil t)
-(add-hook 'before-save-hook #'lsp-format-buffer)
-;; Default Identation
+;;; :completion company
+;; IMO, modern editors have trained a bad habit into us all: a burning need for
+;; completion all the time -- as we type, as we breathe, as we pray to the
+;; ancient ones -- but how often do you *really* need that information? I say
+;; rarely. So opt for manual completion:
+(after! company
+  (setq company-idle-delay nil))
+
+;; fix default identation
 (after! web-mode
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
@@ -109,6 +139,32 @@
   (setq typescript-indent-level 2)
   (setq indent-tabs-mode nil))
 
+;; prodigy
+(map! :leader
+      :desc "Open prodigy"
+      "o p" #'prodigy)
+
+(prodigy-define-tag
+  :name 'phoenix
+  :ready-message "[info] Access SurveyWeb.Endpoint at http://localhost:4000")
+
+(prodigy-define-service
+  :name "Suvery-Server"
+  :command "mix"
+  :args '("phx.server")
+  :cwd "~/Projects/bappeda_dev_team/survey"
+  :env '(("DATABASE_URL" "ecto://postgres:postgres@localhost:54322/survey_dev")
+         ("DATABASE_TEST_URL" "ecto://postgres:postgres@localhost:54322/survey_test")
+         ("PORT" "4000"))
+  :port 4000
+  :tags '(phoenix)
+  :stop-signal 'sigterm
+  )
+
+;; mise
+;; enable globally
+(add-hook 'after-init-hook #'global-mise-mode)
+
 ;; Indent Whole File
 (defun indent-whole-buffer ()
   "INDENT WHOLE BUFFER."
@@ -116,19 +172,111 @@
   (delete-trailing-whitespace)
   (indent-region (point-min) (point-max) nil)
   (untabify (point-min) (point-max)))
-
 (map! :desc "Indent File" :leader "-" #'indent-whole-buffer)
 
-;; hook nov mode
+;; open epub file
 (use-package! nov
   :mode ("\\.epub\\'" . nov-mode)
   :config
   (setq nov-save-place-file (concat doom-cache-dir "nov-places")))
 
+;; clojure
+;; set 'clojure the essential reference' book
+;; as clojure documentation
 (after! cider-mode
   (setq clojure-essential-ref-nov-epub-path "/Users/ry/Projects/clojure-the-essential-references/Clojure_The_Essential_Reference_v31.epub"))
 
-;; (use-package! treemacs-nerd-icons
-;;   :after treemacs
-;;   :config
-;;   (treemacs-load-theme "nerd-icons"))
+;; haskell formatter config
+(after! lsp-haskell
+  (setq lsp-haskell-formatting-provider "ormolu"))
+
+;; projectile find elm dir
+(after! projectile
+  (projectile-register-project-type 'elm '("elm.json")
+                                    :project-file "elm.json"))
+
+;;; :tools lsp
+;; Disable invasive lsp-mode features
+(after! lsp-mode
+  (setq lsp-enable-symbol-highlighting nil
+        ;; If an LSP server isn't present when I start a prog-mode buffer, you
+        ;; don't need to tell me. I know. On some machines I don't care to have
+        ;; a whole development environment for some ecosystems.
+        lsp-enable-suggest-server-download nil
+        ))
+(after! lsp-ui
+  (setq lsp-ui-sideline-enable nil  ; no more useful than flycheck
+        lsp-ui-doc-enable nil))     ; redundant with K
+
+;; elixir
+;; elixir setup lsp
+(add-hook 'elixir-mode-hook #'lsp!)
+(add-to-list 'exec-path "~/.local/share/elixir-ls")
+(after! lsp-mode
+  (setq lsp-elixir-dialyzer-enabled nil))
+;; heex
+(define-derived-mode heex-mode web-mode "HEEx" "Major mode for editing HEEx files")
+(add-to-list 'auto-mode-alist '("\\.heex?\\'" . heex-mode))
+
+(add-hook 'heex-mode-hook #'tree-sitter-hl-mode)
+(add-hook 'heex-mode-hook
+          (lambda()
+            (add-hook 'before-save-hook 'elixir-format nil t)))
+;; elixir repl
+(map! :after elixir-mode
+      :localleader
+      :map elixir-mode-map
+      :prefix ("i" . "inf-elixir")
+      "i" 'inf-elixir
+      "p" 'inf-elixir-project
+      "l" 'inf-elixir-send-line
+      "r" 'inf-elixir-send-region
+      "b" 'inf-elixir-send-buffer
+      "R" 'inf-elixir-reload-module)
+;; ruby
+(setq-hook! 'ruby-mode-hook
+  +format-inhibit t
+  +format-with-lsp t)
+(add-hook! 'ruby-mode-hook
+  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
+
+;; webmode
+(setq-hook! 'web-mode-hook
+  apheleia-inhibit t
+  +format-with nil)
+(add-hook! 'web-mode-hook
+  (lambda()
+    (add-hook 'before-save-hook #'+format/buffer nil t)))
+;; A set of Doom Emacs mappings for manipulating parentheses and sexps
+;; Wrap sexp in round parentheses
+;; `SPC` - `c` -`p` - `(`
+;; Wrap sexp in square parentheses
+;; `SPC` - `c` -`p` - `[`
+;; Wrap sexp in curly parentheses
+;; `SPC` - `c` -`p` - `{`
+;; Copy sexp
+;; `SPC` - `c` -`p` - `y`
+;; Kill sexp
+;; `SPC` - `c` -`p` - `d`
+;; Raise sexp
+;; `SPC` - `c` -`p` - `r`
+;; Push parenthesis to the right
+;; `SPC` - `c` -`p` - `>` - `)`
+;; Push parenthesis to the left
+;; `SPC` - `c` -`p` - `<` - `(`
+;; Pull parenthesis from the right
+;; `SPC` - `c` -`p` - `<` - `)`
+;; Pull parenthesis from the left
+;; `SPC` - `c` -`p` - `>` - `(`
+(use-package! doom-parents)
+;; evil cleverparens
+(add-hook 'lisp-mode-hook #'evil-cleverparens-mode)
+
+;;(use-package! ess-plot :defer t)
+;; gptel
+(setq gptel-model "gpt-4o-mini")
+(menu-bar-mode 0)
+(use-package! openapi-preview :commands 'openapi-preview)
+
+;; mermaid ob
+(setq ob-mermaid-cli-path "/Users/ry/.local/share/mise/installs/node/20/bin/mmdc")
--- doom/init.el
+++ doom/init.el
@@ -13,7 +13,8 @@
 ;;
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
-(setq native-comp-async-jobs-number 1)
+;; (setq native-comp-async-jobs-number 1)
+
 (doom! :input
        ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
@@ -21,23 +22,22 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       (company +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +fuzzy) ;; a search engine for love and life
-       ;; (vertico +icons)
-                                        ; the search engine of the future
+       ;;ivy               ; a search engine for love and life
+       vertico           ; the search engine of the future
 
        :ui
        deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; 🙂
+       (emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;; hydra
+       ;;hydra
        indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
@@ -45,9 +45,9 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       (vc-gutter +pretty +diff-hl) ; vcs diff in the fringe
+       (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
@@ -59,16 +59,16 @@
        fold              ; (nigh) universal code folding
        (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
-       ;; lispy             ; vim for lisp, for people who don't like vim
+       ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       parinfer          ; turn lisp into python, sort of
-       ;;rotate-text       ; cycle region at point between text candidates
+       ;;parinfer          ; turn lisp into python, sort of
+       rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
@@ -89,21 +89,19 @@
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
-       debugger          ; FIXME stepping through code, to help you add bugs
-       ;;direnv
-       docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ein               ; tame Jupyter notebooks with emacs
+       (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
+       direnv
+       (docker +lsp)
+       editorconfig      ; let someone else argue about tabs vs spaces
+       ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
-       lookup              ; navigate your code and its documentation
-       lsp               ; M-x vscode
-       magit             ; a git porcelain for Emacs
+       (lookup +docsets)              ; navigate your code and its documentation
+       (lsp +peek)               ; M-x vscode
+       (magit +forge)             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       ;;pdf               ; pdf enhancements
-       ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
+       pdf               ; pdf enhancements
+       prodigy           ; FIXME managing external services & code builders
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
@@ -111,7 +109,7 @@
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
        tty               ; improve the terminal Emacs experience
 
        :lang
@@ -123,60 +121,59 @@
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
+       data                ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
-       (elixir +lsp)            ; erlang done right
+       (elixir +lsp +tree-sitter)            ; erlang done right
        (elm +lsp +tree-sitter)               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
-       ;;ess               ; emacs speaks statistics
+       (ess +stan +tree-sitter)               ; emacs speaks statistics
        ;;factor
        ;;faust             ; dsp, but you get to keep your soul
        ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       (go +lsp)         ; the hipster dialect
+       (go +lsp +tree-sitter)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
-       ;;(haskell +lsp)    ; a language that's lazier than I am
+       (haskell +lsp +tree-sitter)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
+       (json +tree-sitter)              ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +present +pretty)               ; organize your plain life in plain text
-       ;; (php +lsp +tree-sitter)
-                                        ; perl's insecure younger brother
+       (org +pandoc +present +roam2)               ; organize your plain life in plain text
+       (php +tree-sitter +lsp)               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +conda)            ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
-       ;;rest              ; Emacs as a REST client
+       (rest +jq)          ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       (ruby +rails +lsp +tree-sitter)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       (scala +lsp +tree-sitter)             ; java, but good
+       (ruby +rails +rbenv +lsp +tree-sitter)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web               ; the tubes
-       yaml              ; JSON, but readable
+       (web +tree-sitter)  ; the tubes
+       (yaml +lsp +tree-sitter)                ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
@@ -190,7 +187,6 @@
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
-       ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
        ;;literate
